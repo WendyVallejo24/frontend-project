79f07acdc52f95168468abc593acd605b23ce92d
Wendy Vallejo <wbvalpat24@gmail.com>
2024-03-13 18:26:32 -0600

    update css

--- src/componentes/MenuHamburguesa.js
+++ src/componentes/MenuHamburguesa.js
@@ -9,6 +9,10 @@ const MenuHamburguesa = ({ items, activeIndex, onItemClick }) => {
         setMenuOpen(!menuOpen);
     };
 
+    const closeMenu = () => {
+        setMenuOpen(false);
+    };
+
     return (
         <div>
             <div className={`menu-btn ${menuOpen ? 'open' : ''}`} onClick={toggleMenu} id="menuHam">
@@ -21,27 +25,27 @@ const MenuHamburguesa = ({ items, activeIndex, onItemClick }) => {
                 <ul className='opciones'>
 
                     <li className='titulo'>Pedidos</li>
-                    <Link to='/pedidos' className='no-underline'><li className='opc'>Crear Pedidos</li></Link>
-                    <Link to='/pedidoProceso' className='no-underline'><li className='opc'>Pedidos en Proceso</li></Link>
-                    <Link to='/pedidoEntregado' className='no-underline'><li className='opc'>Pedidos Entregados</li></Link>
-                    <Link to='/pedidoCancelado' className='no-underline'><li className='opc'>Pedidos Cancelados</li></Link>
+                    <Link to='/pedidos' className='no-underline' onClick={closeMenu}><li className='opc'>Crear Pedidos</li></Link>
+                    <Link to='/pedidoProceso' className='no-underline' onClick={closeMenu}><li className='opc'>Pedidos en Proceso</li></Link>
+                    <Link to='/pedidoEntregado' className='no-underline' onClick={closeMenu}><li className='opc'>Pedidos Entregados</li></Link>
+                    <Link to='/pedidoCancelado' className='no-underline' onClick={closeMenu}><li className='opc'>Pedidos Cancelados</li></Link>
 
                     <li className='titulo'>Empleados</li>
-                    <Link to='/registroEmpleado' className='no-underline'><li className='opc'>Registrar Empleado</li></Link>
+                    <Link to='/registroEmpleado' className='no-underline' onClick={closeMenu}><li className='opc'>Registrar Empleado</li></Link>
 
                     <li className='titulo'>Productos</li>
-                    <Link to='/catalogo' className='no-underline'><li className='opc'>Catalogo</li></Link>
-                    <Link to='/agregarProducto' className='no-underline'><li className='opc'>Agregar nuevo</li></Link>
+                    <Link to='/catalogo' className='no-underline' onClick={closeMenu}><li className='opc'>Catalogo</li></Link>
+                    <Link to='/agregarProducto' className='no-underline' onClick={closeMenu}><li className='opc'>Agregar nuevo</li></Link>
 
                     <li className='titulo'>Informe</li>
-                    <Link to='/informeReportes' className='no-underline'><li className='opc'>Reportes</li></Link>
-                    <Link to='/crearReporteSemanal' className='no-underline'><li className='opc'>Reporte Semanal</li></Link>
-                    <Link to='/crearReporteMensual' className='no-underline'><li className='opc'>Reporte Mensual</li></Link>
+                    <Link to='/informeReportes' className='no-underline' onClick={closeMenu}><li className='opc'>Reportes</li></Link>
+                    <Link to='/crearReporteSemanal' className='no-underline' onClick={closeMenu}><li className='opc'>Reporte Semanal</li></Link>
+                    <Link to='/crearReporteMensual' className='no-underline' onClick={closeMenu}><li className='opc'>Reporte Mensual</li></Link>
 
                     <li className='titulo'>Otros</li>
-                    <Link to='/marcas' className='no-underline'><li className='opc'>Listado marcas</li></Link>
-                    <Link to='/categoria' className='no-underline'><li className='opc'>Categoria</li></Link>
-                    <Link to='/unidadMedida' className='no-underline'><li className='opc'>Unidad medida</li></Link>
+                    <Link to='/marcas' className='no-underline' onClick={closeMenu}><li className='opc'>Listado marcas</li></Link>
+                    <Link to='/categoria' className='no-underline' onClick={closeMenu}><li className='opc'>Categoria</li></Link>
+                    <Link to='/unidadMedida' className='no-underline' onClick={closeMenu}><li className='opc'>Unidad medida</li></Link>
 
                     <li className='opc'><CerrarSesion /></li>
                 </ul>
