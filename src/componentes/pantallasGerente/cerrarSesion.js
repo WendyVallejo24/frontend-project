16ba4b93e4c8a99ecdbccb3fe8a312516b727bb0
WendyVallejo24 <wbvalpat24@gmail.com>
2024-07-13 14:34:35 -0600

    update

--- src/componentes/pantallasGerente/cerrarSesion.js
+++ src/componentes/pantallasGerente/cerrarSesion.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
-import './cerrarSesion.css';
+import './style/cerrarSesion.css';
 
 const CerrarSesion = () => {
   const navigate = useNavigate();
